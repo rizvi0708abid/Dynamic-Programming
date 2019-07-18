a3f1885823b259248295065b7825c5c986a821b6
abid72rizvi <52654896+rizvi0708abid@users.noreply.github.com>
2019-07-18 12:38:18 +0530

    1025. Divisor Game
    Alice and Bob take turns playing a game, with Alice starting first.
    
    Initially, there is a number N on the chalkboard.  On each player's turn, that player makes a move consisting of:
    
    Choosing any x with 0 < x < N and N % x == 0.
    Replacing the number N on the chalkboard with N - x.
    Also, if a player cannot make a move, they lose the game.
    
    Return True if and only if Alice wins the game, assuming both players play optimally.
    
     
    
    Example 1:
    
    Input: 2
    Output: true
    Explanation: Alice chooses 1, and Bob has no more moves.
    Example 2:
    
    Input: 3
    Output: false
    Explanation: Alice chooses 1, Bob chooses 1, and Alice has no more moves.

---
+++ DivisorGame.java
@@ -0,0 +1,10 @@
+//https://leetcode.com/problems/divisor-game/
+//1025. Divisor Game
+//BruteForce Solution
+
+class Solution {
+	public boolean divisorGame(int N) {
+        //checking if the number is even or odd 
+        return N%2==0 ? true : false ;  
+    }
+}
